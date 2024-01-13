93d80b21f03b0f873357acabafe97058461882ea
unnamed <adam@ivick.us>
2024-01-13 15:43:35 -0600

    Started work on the home page and made edits to members.

--- script.js
+++ script.js
@@ -3,36 +3,35 @@ let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
 scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY * 1.5;
-})
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
 
-nextBtn.addEventListener("click", ()=> {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 600;
-})
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 500;
+});
 
-backBtn.addEventListener("click", ()=> {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 600;
-})
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 500;
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the current page filename
+  var currentPage = window.location.pathname.split("/").pop();
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the current page filename
-    var currentPage = window.location.pathname.split('/').pop();
+  // Remove leading slash if present
+  if (currentPage.charAt(0) === "/") {
+    currentPage = currentPage.slice(1);
+  }
 
-    // Remove leading slash if present
-    if (currentPage.charAt(0) === '/') {
-        currentPage = currentPage.slice(1);
+  // Find the corresponding link and add the 'active' class
+  var navLinks = document.querySelectorAll("nav a");
+  navLinks.forEach(function (link) {
+    var linkPage = link.getAttribute("href").split("/").pop();
+    if (linkPage === currentPage) {
+      link.parentNode.classList.add("active");
     }
-
-    // Find the corresponding link and add the 'active' class
-    var navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(function (link) {
-        var linkPage = link.getAttribute('href').split('/').pop();
-        if (linkPage === currentPage) {
-            link.parentNode.classList.add('active');
-        }
-    });
+  });
 });
